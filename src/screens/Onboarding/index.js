4d70499b65d468a402413160a5bbfdc958bf6c66
Ajay Sivan <ajayesivan@gmail.com>
2022-06-15 19:12:46 +0530

    Basic screens

---
+++ src/screens/Onboarding/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Onborading = () => {
+  return (
+    <View>
+      <Text>Onboarding</Text>
+    </View>
+  );
+};
+
+export default Onborading;
---
+++ src/screens/Today/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Today = () => {
+  return (
+    <View>
+      <Text>Today</Text>
+    </View>
+  );
+};
+
+export default Today;
