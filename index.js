aae3db8a7f8209dcd21ce6c645ca6c4f00807594
Ajay Sivan <ajayesivan@gmail.com>
2022-06-18 05:38:00 +0530

    Google signin

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import {Navigation} from 'react-native-navigation';
 import React from 'react';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '541837397965-rbh0p6gpd6ed0qqc29dp243cuhig8sks.apps.googleusercontent.com',
+});
 
 import {Onboarding, screenOptions, Today} from '@screens';
 import {Theme} from '@components';
@@ -15,19 +21,36 @@ Navigation.registerComponent('Today', () => props => (
   </Theme>
 ));
 
-Navigation.events().registerAppLaunchedListener(() => {
-  Navigation.setRoot({
-    root: {
-      stack: {
-        children: [
-          {
-            component: {
-              name: 'Onboarding',
-              options: screenOptions,
-            },
+const loginRoot = {
+  root: {
+    stack: {
+      children: [
+        {
+          component: {
+            name: 'Onboarding',
+            options: screenOptions,
           },
-        ],
-      },
+        },
+      ],
     },
-  });
+  },
+};
+
+const homeRoot = {
+  root: {
+    stack: {
+      children: [
+        {
+          component: {
+            name: 'Today',
+            options: screenOptions,
+          },
+        },
+      ],
+    },
+  },
+};
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot((await GoogleSignin.isSignedIn()) ? homeRoot : loginRoot);
 });
--- src/screens/Onboarding/index.js
+++ src/screens/Onboarding/index.js
@@ -1,24 +1,41 @@
-import {Text, TouchableHighlight, View} from '@views';
 import React from 'react';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {Platform} from 'react-native';
+import Snackbar from 'react-native-snackbar';
 import {Navigation} from 'react-native-navigation';
 
+import GoogleLogo from '@assets/images/GoogleLogo';
+import AppleLogo from '@assets/images/AppleLogo';
+import {Text, View} from '@views';
 import {screenOptions} from '@screens';
 
 import Header from './components/Header';
-import GoogleLogo from '@assets/images/GoogleLogo';
-import AppleLogo from '@assets/images/AppleLogo';
-import theme from '@constants/theme';
 import SocialButton from './components/SocialButton';
 
 const Onboarding = props => {
-  const login = () => {
-    Navigation.push(props.componentId, {
-      component: {
-        name: 'Today',
-        options: screenOptions,
-      },
-    });
+  const loginWithGoogle = async () => {
+    try {
+      const {idToken} = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      await auth().signInWithCredential(googleCredential);
+
+      Navigation.push(props.componentId, {
+        component: {
+          name: 'Today',
+          options: screenOptions,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      Snackbar.show({
+        text: 'Error while signing in with Google!',
+        duration: Snackbar.LENGTH_SHORT,
+      });
+    }
   };
+
+  const loginWithApple = () => {};
 
   return (
     <View flex={1} bg="bgGrey">
@@ -30,12 +47,14 @@ const Onboarding = props => {
         color="textPrimary"
         alignItems="center">
         <Text h2>Continue with</Text>
-        <SocialButton mx="m" onPress={login}>
+        <SocialButton mx="m" onPress={loginWithGoogle}>
           <GoogleLogo />
         </SocialButton>
-        <SocialButton onPress={login}>
-          <AppleLogo />
-        </SocialButton>
+        {Platform.OS === 'ios' && (
+          <SocialButton onPress={loginWithApple}>
+            <AppleLogo />
+          </SocialButton>
+        )}
       </View>
       <View height="headerHeight" />
     </View>
